ee6187c90391e8a475ef06db595fbe0668af51dd
Ashley Baldock <ashley@baldock.me>
2017-12-25 17:59:11 +0000

    Global gitignore & add a vim package

--- vim.bundle.install.sh
+++ vim.bundle.install.sh
@@ -9,3 +9,4 @@ git clone https://github.com/ctrlpvim/ctrlp.vim.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/Valloric/YouCompleteMe.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git

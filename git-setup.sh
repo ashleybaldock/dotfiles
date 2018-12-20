cc4baa9bf7935cf9f3f529bfb725d1bc176c7c33
Ashley Baldock <ashley.baldock@bbc.co.uk>
2018-12-20 15:27:02 +0000

    Add some more useful aliases

--- git-setup.sh
+++ git-setup.sh
@@ -1,4 +1,11 @@
-git config --global alias.conflicts "diff --name-only --diff-filter=U"
 git config --global core.editor "/usr/local/bin/vim"
 git config --global core.excludesfile "~/.gitignore"
+
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.conflicts "diff --name-only --diff-filter=U"
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.change-commits "!f() { VAR=$1; OLD=$2; NEW=$3; shift 3; git filter-branch --env-filter \"if [[ \\\"$`echo $VAR`\\\" = '$OLD' ]]; then export $VAR='$NEW'; fi\" $@; }; f "
+
+# git change-commits GIT_AUTHOR_EMAIL "old@email.com" "new@email.com"
+# git change-commits GIT_COMMITTER_EMAIL "old@email.com" "new@email.com" HEAD~10..HEAD
 

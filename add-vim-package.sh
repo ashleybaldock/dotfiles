60044e84c77ba73fd650ea900b3ec24b779a0473
Ashley Baldock <ashley@morepossibility.com>
2022-08-16 15:22:10 +0100

    Modify vim package management to work with tracking specific branch e.g. release for coc

--- add-vim-package.sh
+++ add-vim-package.sh
@@ -1,13 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Run this file from the "dotfiles" directory
 
 # First argument is url, e.g. https://github.com/foo/vim-package.git
 # Second argument is package name, e.g. vim-package
+# (Optional) Third argument is a specific branch (e.g. release) to track
+
+__usage="
+Usage: $(basename $0) <repo-url> <package-name> [<branch>]
+
+Options:
+  -h, --help                   Print this 🤡
+"
+
+if [ $1 = "-h" ] || [ $1 = "--help" ]; then
+  echo 1>&2 "$__usage"
+  exit 2
+elif [ $# -lt 2 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  echo 1>&2 "$__usage"
+  exit 2
+fi
 
 cwd=$(pwd)
 
 git submodule init
-git submodule add $1 .vim/pack/default/start/$2
+if [ $3 != "" ]; then
+  git submodule add -b $3 $1 .vim/pack/default/start/$2
+# Changing existing submodule's branch:
+##   git submodule set-branch --branch release -- .vim/pack/default/start/$2
+else
+  git submodule add $1 .vim/pack/default/start/$2
+fi
 git add .gitmodules .vim/pack/default/start/$2
 git commit -m"Add vim package '$2'"

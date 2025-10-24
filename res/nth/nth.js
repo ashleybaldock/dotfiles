67590361bb2c805414392b139b64c415d131c1bf
Ashley Baldock <ashley@morepossibility.com>
2025-10-24 22:27:47 +0100

    wip

--- res/nth/nth.js
+++ res/nth/nth.js
@@ -4,10 +4,15 @@
   const dataNth = 'nth';
   const pseudoClasses = ['nth-child', 'nth-last-child'];
 
-  const n = [''];
+  const n = ['', 'n'];
   const nthTemplate = css`
-    ${container}:has(> [data-${dataNth}='-n']:hover, > ${row} > :${pseudoClass}(-n):hover)
-  :where([data-${dataNth}='-n'], .nth > :${pseudoClass}(-n)) {
+    ${container}:has(
+      > [data-${dataNth}='-n']:hover,
+      > ${row} > :${pseudoClass}(-n):hover
+    ) :where(
+      [data-${dataNth}='-n'],
+      .nth > :${pseudoClass}(-n)
+    ) {
       --c-badge: #9dd;
       --badge-content: '-n';
     }

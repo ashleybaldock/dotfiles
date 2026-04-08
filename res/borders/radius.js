5605b7429261fc34f5eb0b4c147e1218afdfbc01
Ashley Baldock <ashley@morepossibility.com>
2026-04-08 23:35:15 +0100

    wip

--- res/borders/radius.js
+++ res/borders/radius.js
@@ -6,22 +6,20 @@ readyStateComplete().then(() => {
 
   const sinks = [...document.querySelectorAll('[data-sink]')];
 
+  const setStyleProperty = (element, property, newValue) => {
+    element.style.setProperty(`--bdata-${property}`, newValue);
+    element.style.setProperty(`--bdata-str-${property}`, `'${newValue}'`);
+  };
+
   sinks.forEach((sink) =>
     sink.dataset?.sink.split(' ')
       .flatMap((sourceId) => sourceMap.has(sourceId) ? [[sourceId, sourceMap.get(sourceId)]] : [])
       .forEach(([sourceId, source]) => {
         source.addEventListener('change',
-        ({target}) => {
-          sink.style.setProperty(`--bdata-${sourceId}`, target.value);
-          sink.style.setProperty(`--bdata-str-${sourceId}`, `'${target.value}'`);
-        } );
+        ({target}) => setStyleProperty(sink, sourceId, target.value) );
         source.addEventListener('input',
-        ({target}) => {
-          sink.style.setProperty(`--input-${sourceId}`, target.value);
-          sink.style.setProperty(`--input-str-${sourceId}`, `'${target.value}'`);
-        } );
+        ({target}) => setStyleProperty(sink, sourceId, target.value) );
+        setStyleProperty(sink, sourceId, source.value);
       })
   );
-
-
 });
